f4aa00370e2919c6fe0aa779014fff28e2b9f28b
Starinin Andrey <starininandrey@gmail.com>
2022-10-04 18:32:28 +0300

    Add subquery

--- insert_values.sql
+++ insert_values.sql
@@ -1,11 +1,34 @@
-INSERT INTO table_genres(genre) VALUES ('драма'),
-                                       ('роман'),
-                                       ('фантастика');
+INSERT INTO table_genres(genre)
+VALUES ('драма'),
+       ('роман'),
+       ('фантастика');
 
-INSERT INTO table_authors(last_name, first_name) VALUES ('Толстой', 'Лев'),
-                                                        ('Пушкин', 'Александр'),
-                                                        ('Толстой', 'Алексей');
+INSERT INTO table_authors(last_name, first_name)
+VALUES ('Толстой', 'Лев'),
+       ('Пушкин', 'Александр'),
+       ('Толстой', 'Алексей');
 
-INSERT INTO table_books(title, genre_id, author_id) VALUES ('Аэлита', 3, 3),
-                                                           ('Война и мир', 2, 1),
-                                                           ('Пиковая дама', 1, 2);
+INSERT INTO table_books(title, genre_id, author_id)
+VALUES ('Аэлита',
+        (SELECT genre_id
+         FROM table_genres
+         WHERE genre = 'фантастика'),
+        (SELECT author_id
+         FROM table_authors
+         WHERE last_name = 'Толстой'
+           AND first_name = 'Алексей')),
+       ('Война и мир',
+        (SELECT genre_id
+         FROM table_genres
+         WHERE genre = 'роман'),
+        (SELECT author_id
+         FROM table_authors
+         WHERE last_name = 'Толстой'
+           AND first_name = 'Лев')),
+       ('Пиковая дама',
+        (SELECT genre_id
+         FROM table_genres
+         WHERE genre = 'драма'),
+        (SELECT author_id
+         FROM table_authors
+         WHERE last_name = 'Пушкин'));
